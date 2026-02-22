4d157a2adc68886708409ba6fb8a1ae9175a840f
arnavkumar6656-beep <arnavkumar6656@gmail.com>
2026-02-22 21:39:48 +0530

    Delete popup.js

--- popup.js
+++
@@ -1,8 +0,0 @@
-function sendMode(mode) {
-  chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
-    chrome.runtime.sendMessage({ mode, tabId: tab.id });
-  });
-}
-
-document.getElementById("simplify").onclick = () => sendMode("simplify");
-document.getElementById("focus").onclick = () => sendMode("focus");
